39e8ab6d05ea8a9154c1f1f77a123426d8f8bff9
Isaac Blanco <nuclearhands2001@gmail.com>
2026-03-23 01:15:13 -0500

    refactor: lifespan for quick load

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pathlib
 import uvicorn
 from google.adk.cli.fast_api import get_fast_api_app
 from config import Config
+from contextlib import asynccontextmanager
+from tools import scrapper
+from fastapi import FastAPI
 
 AGENTS_DIR = pathlib.Path(__file__).parent
 config = Config()
@@ -18,6 +21,14 @@ app = get_fast_api_app(
     session_service_uri=config.session_service_uri,
     web=config.web_allowed,
 )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scrapper.open_airports_connection(config.airports_sqlite_path)
+    yield
+    scrapper.close_airports_connection()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=config.port)
